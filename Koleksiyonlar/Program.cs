5adcafb48caaf737d55e6d27cb83f02436a384df
Çağrı Tanyıldızı <cgrtnyldz@hotmail.com>
2021-01-22 22:22:23 +0300

    20 Ocak ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "çağrı", "engin", "murat" };
+            //console.writeline(isimler[0]);
+            //console.writeline(isimler[1]);
+            //console.writeline(isimler[2]);
+
+            List<string> isimler2 = new List<string> {"Çağrı", "Engin", "Murat" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+            
+        }
+    }
+}
